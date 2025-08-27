6871f6f3a1029cfcf1b8757aa5305d5a94c52fca
barisemreozturk <barisemreozturk@outlook.com.tr>
2025-08-27 15:36:53 +0300

    postgres: az variable added

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_postgresql_flexible_server" "main" {
   storage_mb                    = var.storage_mb
   storage_tier                  = var.storage_tier
   auto_grow_enabled             = var.auto_grow_enabled
-  zone                          = "1"
+  zone                          = var.zone
 
   dynamic "high_availability" {
     for_each = (var.high_availability == true) ? [true] : []
